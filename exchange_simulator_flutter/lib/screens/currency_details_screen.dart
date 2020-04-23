a0e10b674a26e950d4c01608b4f8ab4e6e9e27b6
jmysliv <jjmreal34@gmail.com>
2020-04-23 18:52:11 +0200

    added displaying timestamp info in chart

--- exchange_simulator_flutter/lib/screens/currency_details_screen.dart
+++ exchange_simulator_flutter/lib/screens/currency_details_screen.dart
@@ -1,13 +1,15 @@
+import 'dart:math';
 import 'package:exchange_simulator_flutter/bloc/currency_details/currency_detail.dart';
 import 'package:exchange_simulator_flutter/models/currency_model.dart';
 import 'package:exchange_simulator_flutter/repositories/bet_repository.dart';
 import 'package:exchange_simulator_flutter/repositories/currency_repository.dart';
 import 'package:exchange_simulator_flutter/repositories/user_repository.dart';
 import 'package:exchange_simulator_flutter/screens/error_screen.dart';
-import 'package:exchange_simulator_flutter/screens/loading_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:charts_flutter/flutter.dart';
+import 'package:charts_flutter/src/text_element.dart' as TextElement;
+import 'package:charts_flutter/src/text_style.dart' as style;
 import 'package:intl/intl.dart';
 
 
@@ -48,6 +50,7 @@ class CurrencyDetailScreen extends StatelessWidget {
 
 class CurrencyChart extends StatefulWidget{
   final Currency currency;
+  static Timestamp timestampPointer;
 
   CurrencyChart(this.currency);
 
@@ -55,6 +58,7 @@ class CurrencyChart extends StatefulWidget{
 }
 
 class _CurrencyChartState extends State<CurrencyChart>{
+
   @override
   void initState() {
     super.initState();
@@ -72,39 +76,89 @@ class _CurrencyChartState extends State<CurrencyChart>{
         title: Text("${widget.currency.name}"),
       ),
       body: Container(
-        child: charts.TimeSeriesChart(
-          [
-            charts.Series<Timestamp, DateTime>(
-              id: 'Exchange rate',
-              colorFn: (_, __) => charts.ColorUtil.fromDartColor(Colors.cyan),
-              domainFn: (Timestamp timestamp, _) => timestamp.date,
-              measureFn: (Timestamp timestamp, _) => timestamp.exchangeRate,
-              data: widget.currency.timestamps,
-            ),
+        child: ListView(
+          children: <Widget>[
+            displayChart(context)
           ],
-          animate: true,
-          animationDuration: Duration(seconds: 1),
-          behaviors: [
-            new charts.SlidingViewport(),
-          ],
-          primaryMeasureAxis: charts.NumericAxisSpec(
-              tickProviderSpec: charts.BasicNumericTickProviderSpec(zeroBound: false, desiredTickCount: 5, dataIsInWholeNumbers: false),
-              tickFormatterSpec: charts.BasicNumericTickFormatterSpec.fromNumberFormat(NumberFormat.currency(locale: 'PL'))),
-          domainAxis: new charts.DateTimeAxisSpec(
-              tickProviderSpec: charts.DayTickProviderSpec(increments: [80])),
-//          selectionModels: [
-//            charts.SelectionModelConfig(
-//              type: charts.SelectionModelType.info,
-//              changedListener:(context){},
-//            )
-//          ],
-          dateTimeFactory: const charts.LocalDateTimeFactory(),
-
-        ),
+        )
       ),
     );
   }
 
 
+  Widget displayChart(BuildContext context){
+    return  TimeSeriesChart(
+      [
+        Series<Timestamp, DateTime>(
+          id: 'Exchange rate',
+          colorFn: (_, __) => ColorUtil.fromDartColor(Colors.cyan),
+          domainFn: (Timestamp timestamp, _) => timestamp.date,
+          measureFn: (Timestamp timestamp, _) => timestamp.exchangeRate,
+          data: widget.currency.timestamps,
+        ),
+      ],
+      animate: true,
+      animationDuration: Duration(seconds: 1),
+      behaviors: [
+        LinePointHighlighter(
+            symbolRenderer: RenderTimestampInfo()
+        )
+      ],
+      primaryMeasureAxis: NumericAxisSpec(
+          tickProviderSpec: BasicNumericTickProviderSpec(zeroBound: false, desiredTickCount: 5, dataIsInWholeNumbers: false),
+          tickFormatterSpec: BasicNumericTickFormatterSpec.fromNumberFormat(NumberFormat.currency(locale: 'PL'))),
+      domainAxis: DateTimeAxisSpec(
+          tickProviderSpec: DayTickProviderSpec(increments: [80])),
+      selectionModels: [
+        SelectionModelConfig(
+          changedListener: (SelectionModel model) {
+            if(model.hasDatumSelection){
+              CurrencyChart.timestampPointer = model.selectedDatum.first.datum as Timestamp;
+            }
+          }
+        )
+      ],
+      dateTimeFactory: const LocalDateTimeFactory(),
 
+    );
+  }
+
+  Widget displayCurrencyInfo(BuildContext context){
+    return 
+  }
+
+}
+
+class RenderTimestampInfo extends CircleSymbolRenderer {
+  @override
+  void paint(ChartCanvas canvas, Rectangle<num> bounds, {List<int> dashPattern, Color fillColor, FillPatternType fillPattern, Color strokeColor, double strokeWidthPx}) {
+    super.paint(canvas, bounds, dashPattern: dashPattern, fillColor: fillColor, strokeColor: strokeColor, strokeWidthPx: strokeWidthPx, fillPattern: fillPattern);
+    canvas.drawRect(
+        Rectangle(bounds.left - 45, bounds.top - 50, bounds.width + 80, bounds.height + 20),
+        fill: Color.fromHex( code: "#1f1f1f")
+    );
+    var textStyle = style.TextStyle();
+    textStyle.color = Color.white;
+    textStyle.fontSize = 8;
+    canvas.drawText(
+        TextElement.TextElement("Kurs: ", style: textStyle),
+        (bounds.left - 30).round(),
+        (bounds.top - 46).round()
+    );
+    canvas.drawText(
+        TextElement.TextElement("${CurrencyChart.timestampPointer.exchangeRate.toStringAsFixed(3)}", style: textStyle),
+        (bounds.left - 5).round(),
+        (bounds.top - 46).round()
+    );
+    canvas.drawText(
+        TextElement.TextElement("Data: ", style: textStyle),
+        (bounds.left - 30).round(),
+        (bounds.top - 36).round()
+    );
+    canvas.drawText(
+        TextElement.TextElement("${DateFormat.yMd().format(CurrencyChart.timestampPointer.date)}", style: textStyle),
+        (bounds.left - 5).round(),
+        (bounds.top - 36).round()
+    );
+  }
 }
